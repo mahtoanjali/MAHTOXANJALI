06016611cdd069260cf01e4b2c86691f465bef1d
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-05-26 20:18:54 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/itz-star-boi/ShizukaXMusic"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/WCGKING/BRANDEDXSHIZUKA"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/itz-star-boi/ShizukaXMusic"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/WCGKING/BRANDEDXSHIZUKA"),
                 ]
             )
         if OWNER:
