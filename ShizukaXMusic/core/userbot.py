61989d0397358c585275052b7d5710ce5b0b2f5c
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-28 13:05:38 +0530

    Update userbot.py

--- ShizukaXMusic/core/userbot.py
+++ ShizukaXMusic/core/userbot.py
@@ -48,9 +48,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(1)
@@ -80,9 +80,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.two.join_chat("BRANDED_PAID_CC")
+                await self.two.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.two.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(2)
@@ -112,9 +112,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.three.join_chat("BRANDED_PAID_CC")
+                await self.two.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.three.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(3)
@@ -144,9 +144,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.four.join_chat("BRANDED_PAID_CC")
+                await self.two.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.four.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(4)
@@ -176,9 +176,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.five.join_chat("BRANDED_PAID_CC")
+                await self.two.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.five.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(5)
@@ -195,7 +195,7 @@ class Userbot(Client):
                 self.five.name = get_me.first_name
             try:
                 await self.five.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғɪᴠᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.five.id}`\n❄ ɴᴀᴍᴇ : {self.five.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.five.username}"
+                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғɪᴠᴇ sᴛᴀʀᴛᴇᴅ :**\n\n ɪᴅ : `{self.five.id}`\n❄ ɴᴀᴍᴇ : {self.five.name}\n ᴜsᴇʀɴᴀᴍᴇ : @{self.five.username}"
                 )
             except:
                 LOGGER(__name__).error(
