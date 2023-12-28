a793b9e55788d633c50d53564bee8f3ceb564bbe
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-28 20:53:54 +0530

    Update __main__.py

--- ShizukaXMusic/__main__.py
+++ ShizukaXMusic/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await Shizuka.start()
     try:
-        await Shizuka.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Shizuka.stream_call("https://te.legra.ph/file/a097a5ba79a8982845e37.mp4")
     except NoActiveGroupCall:
         LOGGER("ShizukaXMusic").error(
             "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
