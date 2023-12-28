5d7b99a1de0c15cc4e6ecc9b95c4ac4ebd6a7622
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-28 14:00:02 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/WCGKING/BRANDEDXSHIZUKA"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/systemx5/MAHTOXANJALI"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/WCGKING/BRANDEDXSHIZUKA"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/systemx5/MAHTOXANJALI"),
                 ]
             )
         if OWNER:
